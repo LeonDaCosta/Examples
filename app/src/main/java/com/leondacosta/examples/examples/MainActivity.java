df6d85ddb2371bbde6c00a9bea90850f2151876a
Leon Da Costa <leon@onbrandgroup.co.uk>
2017-04-24 17:22:55 +0100

    Add function to check if headphone are plugged in.

--- app/src/main/java/com/leondacosta/examples/examples/MainActivity.java
+++ app/src/main/java/com/leondacosta/examples/examples/MainActivity.java
@@ -1,6 +1,13 @@
 package com.leondacosta.examples.examples;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,8 +16,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private MusicIntentReceiver myReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +39,31 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-    // Example of a call to a native method
-    TextView tv = (TextView) findViewById(R.id.sample_text);
-    tv.setText(stringFromJNI());
+        // Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+
+        TextView tv2 = (TextView) findViewById(R.id.sample_text2);
+
+        if (isNetworkAvailable()) {
+            tv2.setText("There is an internet connection");
+        }
+
+        myReceiver = new MusicIntentReceiver();
+
+    }
+
+    @Override
+    public void onResume() {
+        IntentFilter filter = new IntentFilter(Intent.ACTION_HEADSET_PLUG);
+        registerReceiver(myReceiver, filter);
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        unregisterReceiver(myReceiver);
+        super.onPause();
     }
 
     @Override
@@ -64,5 +97,34 @@ public class MainActivity extends AppCompatActivity {
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+
+    private class MusicIntentReceiver extends BroadcastReceiver {
+        @Override public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(Intent.ACTION_HEADSET_PLUG)) {
+                int state = intent.getIntExtra("state", -1);
+                switch (state) {
+                    case 0:
+                        Log.d(TAG, "Headset is unplugged");
+                        Toast.makeText(MainActivity.this,"Headset is unplugged", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 1:
+                        Log.d(TAG, "Headset is plugged");
+                        Toast.makeText(MainActivity.this,"Headset is plugged", Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        Log.d(TAG, "I have no idea what the headset state is");
+                        Toast.makeText(MainActivity.this,"I have no idea what the headset state is", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
     }
 }
