48d1ce72aeabdd0e3e7ce56886f3a9ccc965a689
leondacosta <leon@dacosta.me>
2017-04-29 21:38:29 +0100

    Added VoiceRecognitionActivity class

---
+++ app/src/main/java/com/leondacosta/examples/examples/VoiceRecognitionActivity.java
@@ -0,0 +1,13 @@
+package com.leondacosta.examples.examples;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VoiceRecognitionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voice_recognition);
+    }
+}
